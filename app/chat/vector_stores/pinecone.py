400956d34fbe4f68f31f38ee605465f79330be5b
Matteo Vannini <136280542+heisenbrook@users.noreply.github.com>
2026-02-26 10:09:00 +0000

    Modified requirements.txt

--- app/chat/vector_stores/pinecone.py
+++ app/chat/vector_stores/pinecone.py
@@ -1,11 +1,12 @@
 import os
-import pinecone
 from langchain.vectorstores.pinecone import Pinecone
 from app.chat.embeddings.openai import embeddings
 
-pinecone.Pinecone(
+Pinecone(
     api_key=os.getenv('PINECONE_API_KEY'),
     environment=os.getenv('PINECONE_ENV_NAME')
 )
 
-vector_store = 
+vector_store = Pinecone.from_existing_index(
+
+)
